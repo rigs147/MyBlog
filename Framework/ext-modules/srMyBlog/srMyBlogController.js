323c7cbcd649e2c475f9423b008248b51637e965
rigs147 <seanrigney@hotmail.co.uk>
2015-07-09 17:51:38 +0100

    update

--- Framework/ext-modules/srMyBlog/srMyBlogController.js
+++ Framework/ext-modules/srMyBlog/srMyBlogController.js
@@ -1,6 +1,6 @@
 ﻿"use strict";
 
-angular.module("srMyBlog").controller("srMyBlogController", ['$scope', '$rootScope', '$window', function ($scope, $rootScope, $window) {
+angular.module("srMyBlog").controller("srMyBlogController", ['$scope', '$rootScope', '$window', '$timeout', function ($scope, $rootScope, $window, $timeout) {
 
     $scope.isMenuVisible = true;
 
@@ -12,19 +12,24 @@ angular.module("srMyBlog").controller("srMyBlogController", ['$scope', '$rootSco
     });
 
     $($window).on('resize.srMyBlog', function() {
-        $scope.apply(function() {
+        $scope.$apply(function() {
             checkWidth();
         });
     });
 
     $scope.$on("$destroy", function() {
-        $($window).off("resize.srMyBlog");// remove the handler
-    })
+        $($window).off("resize.srMyBlog"); // remove the handler
+    });
 
     var checkWidth = function() {
-        var width = $($window).innerWidth();
+        //var width = $($window).innerWidth();
+        var width = Math.max($($window).width(), $($window).innerWidth());
         $scope.isMenuVisible = (width > 768);
         $scope.isMenuButtonVisible = !$scope.isMenuVisible;
-    }
+    };
+
+    $timeout(function() {
+        checkWidth();
+    }, 0);
 
 }]);
